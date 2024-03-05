8830e4a5a9d76565fd88dcace078cb560a97b53b
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-05 14:51:28 -0800

    update board representation

---
+++ abaai-client/src/components/Game.jsx
@@ -0,0 +1,21 @@
+import { BoardLayouts } from "../constants/boardLayouts";
+import { Board } from "./Board";
+import ConfigMenu from "./ConfigMenu";
+import { useState } from "react";
+import { useBoard } from "../hooks/useBoard";
+
+const Game = () => {
+  const [config, setConfig] = useState({
+    boardLayout: BoardLayouts.DEFAULT,
+  });
+  const { board } = useBoard(config.boardLayout); // import board state and setBoard function from useBoard hook
+
+  return (
+    <>
+      <Board boardArray={board} />
+      <ConfigMenu config={config} setConfig={setConfig} />
+    </>
+  );
+};
+
+export default Game;
