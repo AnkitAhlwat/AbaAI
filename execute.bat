457a1bb434bfa758b8b01e10c0088fd61423ea8b
Langelozzi <lucasangelozzi32@gmail.com>
2024-03-07 12:46:16 -0800

    remove pause

--- execute.bat
+++ execute.bat
@@ -15,19 +15,17 @@ call venv\Scripts\activate
 echo Installing requirements
 pip install -r requirements.txt
 
-rem Start the backend server in a separate command prompt window
-echo Starting the backend server
+rem Start the backend server
 start /MIN cmd /C "python app.py"
 
 rem Start the react server
 echo Starting the react server
 
+rem Change directory to the abaai-client directory
 cd ../abaai-client
 
 echo Installing node modules
 npm install
 
-echo Starting the frontend server
+rem Start the frontend server
 start /MIN cmd /C "npm run dev"
-
-pause
