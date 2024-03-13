75004e8305c16d5081cb9b6383866985aa7c973c
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-13 15:27:49 -0700

    Add sidebar template

---
+++ abaai-client/src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+const SideBar = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#262522",
+        borderRadius: "5px",
+        height: "100%",
+      }}
+    ></Box>
+  );
+};
+
+export default SideBar;
