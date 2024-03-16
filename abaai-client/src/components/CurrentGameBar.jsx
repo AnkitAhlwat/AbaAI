7bbfaddf05e89e950c441abdc24e3be23911f6b8
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-15 22:00:39 -0700

    Update move history ui

--- abaai-client/src/components/CurrentGameBar.jsx
+++ abaai-client/src/components/CurrentGameBar.jsx
@@ -7,6 +7,44 @@ import { Divider, Typography } from "@mui/material";
 const CurrentGameBar = (props) => {
   const { movesStack, aiMove } = props;
 
+  const dummyMovesStack = [
+    {
+      player: 1,
+      previous_positions: [
+        { x: 2, y: 6 },
+        { x: 3, y: 6 },
+      ],
+      next_positions: [
+        { x: 2, y: 5 },
+        { x: 3, y: 5 },
+      ],
+    },
+    {
+      player: 2,
+      previous_positions: [
+        { x: 4, y: 2 },
+        { x: 5, y: 2 },
+      ],
+      next_positions: [
+        { x: 4, y: 3 },
+        { x: 5, y: 3 },
+      ],
+    },
+    {
+      player: 1,
+      previous_positions: [
+        { x: 4, y: 6 },
+        { x: 4, y: 7 },
+        { x: 4, y: 8 },
+      ],
+      next_positions: [
+        { x: 4, y: 5 },
+        { x: 4, y: 6 },
+        { x: 4, y: 7 },
+      ],
+    },
+  ];
+
   const centerDivider = (text) => {
     if (text === "" || text === undefined) {
       return <Divider variant="middle" sx={{ bgcolor: "white" }} />;
@@ -32,7 +70,7 @@ const CurrentGameBar = (props) => {
       {centerDivider("AI Suggested Move")}
       <AIMoveDisplay aiMove={aiMove} />
       {centerDivider("Move History")}
-      <MoveHistory movesStack={movesStack} />
+      <MoveHistory movesStack={dummyMovesStack} />
     </>
   );
 };
