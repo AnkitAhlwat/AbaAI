0708fc6508a2c4820904fd5563f14f9f6e05925c
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-15 21:28:53 -0700

    update ai move suggestion ui

--- abaai-client/src/components/CurrentGameBar.jsx
+++ abaai-client/src/components/CurrentGameBar.jsx
@@ -1,17 +1,45 @@
 import MoveHistory from "./MoveHistory";
 import AIMoveDisplay from "./AiMove";
 import GameClock from "./Clock";
+import Proptypes from "prop-types";
+import { Divider, Typography } from "@mui/material";
 
 const CurrentGameBar = (props) => {
   const { movesStack, aiMove } = props;
 
+  const centerDivider = (text) => {
+    if (text === "" || text === undefined) {
+      return <Divider variant="middle" sx={{ bgcolor: "white" }} />;
+    }
+    return (
+      <Divider
+        variant="middle"
+        sx={{
+          color: "#f5f5f5",
+          "&::before, &::after": {
+            borderColor: "#f5f5f5",
+          },
+        }}
+      >
+        <Typography>{text}</Typography>
+      </Divider>
+    );
+  };
+
   return (
     <>
       <GameClock />
+      {centerDivider("AI Suggested Move")}
       <AIMoveDisplay aiMove={aiMove} />
+      {centerDivider("Move History")}
       <MoveHistory movesStack={movesStack} />
     </>
   );
+};
+
+CurrentGameBar.propTypes = {
+  movesStack: Proptypes.array.isRequired,
+  aiMove: Proptypes.string.isRequired,
 };
 
 export default CurrentGameBar;
