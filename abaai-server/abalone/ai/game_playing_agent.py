37d47979f04a0a8f12b99865b4041ba1fef309b4
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2024-03-24 13:10:35 -0700

    Add layout of heuristic functions

--- abaai-server/abalone/ai/game_playing_agent.py
+++ abaai-server/abalone/ai/game_playing_agent.py
@@ -1,5 +1,5 @@
 from abalone.ai.state_space_generator import StateSpaceGenerator
-from abalone.movement import Move
+from abalone.movement import Move, Position
 from abalone.state import GameStateUpdate, GameState
 
 
@@ -42,5 +42,87 @@ class MiniMaxAgent:
             return min_eval
 
     def evaluate(self, game_state: GameState) -> float:
-        # Write our own evaluation function
-        pass
+        # NOTE: Any function that is a good thing will add to value, bad things will subtract from value
+        value = 0
+
+        # Get the positions of the min and max player pieces
+        player_piece_positions = StateSpaceGenerator.get_player_piece_positions(game_state)
+        max_player_piece_positions = player_piece_positions["player_max"]
+        min_player_piece_positions = player_piece_positions["player_min"]
+
+        # 1. Terminal state: if player won large positive, if opponent won small negative, if game not over 0
+        value += MiniMaxAgent.terminal_state(game_state)
+
+        # 2. Piece counts: 1 point for each player piece, -1 point for each opponent piece
+        value += MiniMaxAgent.piece_counts(game_state)
+
+        # 3. Pieces near edge: can our pieces be pushed off soon, can we push off opponent pieces soon
+
+        # 4. Manhattan distance from center: +(4 - distance) player pieces, -(4 - distance) for opponent pieces
+        value += MiniMaxAgent.manhattan_distance(max_player_piece_positions, min_player_piece_positions)
+
+        # 5. Clumping: get our pieces close together, spread out opponent pieces
+
+        # 6. Sumito: how many sumito match ups are there, how many can we win
+
+        # 7. Triples: number of triples we have, number of triples opponent has
+
+        # 8. Doubles: number of doubles we have, number of doubles opponent has
+
+        return value
+
+    # Heuristic functions
+    @staticmethod
+    def terminal_state(game_state):
+        """
+        If the current player won return a large positive value, if the opponent won return a small negative value,
+        if the game is not over yet, return 0.
+
+        :param game_state: the current game state
+        :return: a float representing the value of the terminal state
+        """
+        starting_marble_count = 14
+        num_marbles_pushed_off_to_win = 6
+        loser_marble_count = starting_marble_count - num_marbles_pushed_off_to_win
+
+        # if the player wins
+        if game_state.remaining_opponent_marbles <= loser_marble_count:
+            return 100_000
+        # if the opponent wins
+        elif game_state.remaining_player_marbles <= loser_marble_count:
+            return -100_000
+        # if the game is not over yet
+        else:
+            return 0
+
+    @staticmethod
+    def piece_counts(game_state):
+        """
+        For each player piece, add 1 point, for each opponent piece, subtract 1 point.
+
+        :param game_state: the current game state
+        :return: a float representing the value of the piece counts
+        """
+        return game_state.remaining_player_marbles - game_state.remaining_opponent_marbles
+
+    @staticmethod
+    def manhattan_distance(
+            player_piece_positions: list[Position],
+            opponent_piece_positions: list[Position]
+    ) -> float:
+        player_value = 0
+        opponent_value = 0
+        for player_position in player_piece_positions:
+            player_value += MiniMaxAgent.__manhattan_distance_from_center(player_position)
+        for opponent_position in opponent_piece_positions:
+            opponent_value += MiniMaxAgent.__manhattan_distance_from_center(opponent_position)
+
+        # the smaller value the better
+        # e.g. player distance = 2, opponent distance = 4, the value should be +2 because player is closer to the center
+        return float(opponent_value - player_value)
+
+    # Private helper functions
+    @staticmethod
+    def __manhattan_distance_from_center(position: Position):
+        center_position_x, center_position_y = 4, 4
+        return abs(position.x - center_position_x) + abs(position.y - center_position_y)
