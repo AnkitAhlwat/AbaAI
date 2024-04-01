de5baece9a4ff137e94c13ba1d71a228084d83f0
Harrison de Jong <harrisondijon@gmail.com>
2024-04-01 14:22:47 -0700

    Adding changes for electron app executable

---
+++ abaai-client/electron-starter.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  const startURL = 'http://localhost:5173';
+
+  mainWindow.loadURL(startURL);
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
