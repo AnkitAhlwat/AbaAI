dee168f33fb18b576585534c048381738a74af9f
Harrison de Jong <harrisondijon@gmail.com>
2024-04-03 15:30:54 -0700

    Corrected executable files

--- abaai-client/electron-starter.cjs
+++ abaai-client/electron-starter.cjs
@@ -1,7 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 const { spawn } = require('child_process');
 
+let mainWindow;
 let flaskProcess = null;
+let viteServer = null;
+
+function createViteServer() {
+  viteServer = spawn('npm', ['run', 'dev'], { shell: true, stdio: 'inherit', cwd: __dirname });
+
+  viteServer.on('close', code => {
+    console.log(`Vite server exited with code ${code}`);
+  });
+}
+
+function runServer() {
+  flaskProcess = spawn('./app.exe', [], { shell: true });
+  flaskProcess.stdout.on('data', data => {
+    console.log(`Flask: ${data}`);
+  });
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -14,28 +31,29 @@ function createWindow() {
   });
 
   const startURL = 'http://localhost:5173';
-
   mainWindow.loadURL(startURL);
-  mainWindow.on('closed', function () {
+
+  mainWindow.on('closed', () => {
     mainWindow = null;
+    if (flaskProcess) flaskProcess.kill();
+    if (viteServer) viteServer.kill();
   });
-// Start Flask server
-flaskProcess = spawn('path/to/your/flask/executable', [], { shell: true });
-flaskProcess.stdout.on('data', (data) => {
-  console.log(`Flask: ${data}`);
-});
+
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  runServer();
+  createViteServer();
+  createWindow();
+});
 
-app.on('window-all-closed', function () {
-  if (flaskProcess) flaskProcess.kill();
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
