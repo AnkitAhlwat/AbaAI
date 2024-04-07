8aaae0a4bb3b5d88d43e342e0716778fe1e3e99f
ankitahlwat <ankitahlwat54@gmail.com>
2024-04-07 16:04:15 -0700

    Testing

--- abaai-server/abalone/ai/agent_revamped_clumping_revamped.py
+++ abaai-server/abalone/ai/agent_revamped_clumping_revamped.py
@@ -285,19 +285,31 @@ MANHATTAN_WEIGHT_CONVERTED = [
 def simulate_agents(game_state: GameState, max_moves: int):
     game_state = game_state
     agent1 = AlphaBetaPruningAgentIterative(max_depth=4)
-    # agent2 = AlphaBetaPruningAgentNoClumping(max_depth=4)
-    agent2 = alphaBetaPruningAgentClumping(game_state, max_depth=4)
+    agent2 = AlphaBetaPruningAgentNoClumping(max_depth=4)
 
     for i in range(max_moves):
         if game_state.turn.value == 2:
+            original_marbles = game_state.remaining_opponent_marbles
+            original_opponent_marbles = game_state.remaining_player_marbles
             best_move = agent1.iterative_deepening_search(game_state)
             game_state = GameStateUpdate(game_state, best_move).resulting_state
-            print(f'black move {best_move}')
+            print(f'white move {best_move}')
+            if game_state.remaining_player_marbles < original_marbles:
+                print(f'marbles knocked off')
+            if game_state.remaining_opponent_marbles < original_opponent_marbles:
+                print(f'marbles knocked off')
         else:
-            agent2 = alphaBetaPruningAgentClumping(game_state, max_depth=4)
-            move = agent2.AlphaBetaPruningSearch()
+            # agent2 = alphaBetaPruningAgentClumping(game_state, max_depth=4)
+            # move = agent2.AlphaBetaPruningSearch()
+            original_marbles = game_state.remaining_opponent_marbles
+            original_opponent_marbles = game_state.remaining_player_marbles
+            move = agent2.iterative_deepening_search(game_state)
             game_state = GameStateUpdate(game_state, move).resulting_state
-            print(f'white move {move}')
+            print(f'black move {move}')
+            if game_state.remaining_player_marbles < original_marbles:
+                print(f'marbles knocked off')
+            if game_state.remaining_opponent_marbles < original_opponent_marbles:
+                print(f'marbles knocked off')
 
     print(game_state.turn)
     print(game_state.remaining_player_marbles)
@@ -339,5 +351,5 @@ def simulate_moves(game_state: GameState, max_moves: int):
 if __name__ == '__main__':
     # simulate_moves(GameState(), 10)
     # simulate_moves(GameState(OptimizedBoard(BoardLayout.GERMAN_DAISY.value)), 1)
-    simulate_agents(GameState(),50)
+    simulate_agents(GameState(),80)
     # simulate_moves(GameState(), 150)
