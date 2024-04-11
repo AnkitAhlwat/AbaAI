06547e679fcc1e48862bc06d2592566148d58ad1
ankitahlwat <ankitahlwat54@gmail.com>
2024-04-10 17:34:59 -0700

    Testing cython file

--- abaai-server/abalone/ai/simulate_agents.py
+++ abaai-server/abalone/ai/simulate_agents.py
@@ -1,4 +1,6 @@
-from abalone.ai.ankit_agent import AlphaBetaPruningAgentAnkit
+import time
+
+from abalone.ai.cython.cython_clumping_agent import AlphaBetaPruningAgentAnkit
 from abalone.board import OptimizedBoard
 from abalone.state import GameState, GameStateUpdate
 from abalone.ai.game_playing_agent_revamped_iterative_deepening import AlphaBetaPruningAgentIterative
@@ -11,11 +13,13 @@ def simulate_agents(game_state: GameState, max_moves: int):
 
     for i in range(max_moves):
         if game_state.turn.value == 1:
+            start = time.time()
             original_marbles = game_state.remaining_opponent_marbles
             original_opponent_marbles = game_state.remaining_player_marbles
             best_move = agent1.iterative_deepening_search(game_state)
             game_state = GameStateUpdate(game_state, best_move).resulting_state
             print(f'black move {best_move}')
+            print(f'time taken: {time.time() - start}')
             if game_state.remaining_player_marbles < original_marbles:
                 print(f'marbles knocked off')
             if game_state.remaining_opponent_marbles < original_opponent_marbles:
@@ -38,4 +42,5 @@ def simulate_agents(game_state: GameState, max_moves: int):
     # agent2.write_t_table()
 
 if __name__ == '__main__':
-    simulate_agents(GameState(OptimizedBoard([[-1, -1, -1, -1, 0, 0, 0, 2, 2], [-1, -1, -1, 2, 0, 0, 0, 2, 2], [-1, -1, 0, 0, 2, 0, 2, 2, 0], [-1, 0, 0, 2, 1, 2, 2, 0, 0], [0, 0, 0, 1, 1, 2, 2, 0, 0], [0, 0, 1, 1, 1, 2, 0, 0, -1], [0, 1, 1, 1, 1, 1, 0, -1, -1], [0, 1, 0, 0, 0, 1, -1, -1, -1], [0, 1, 0, 0, 0, -1, -1, -1, -1]])), 2)
+    # simulate_agents(GameState(OptimizedBoard([[-1, -1, -1, -1, 0, 0, 0, 2, 2], [-1, -1, -1, 2, 0, 0, 0, 2, 2], [-1, -1, 0, 0, 2, 0, 2, 2, 0], [-1, 0, 0, 2, 1, 2, 2, 0, 0], [0, 0, 0, 1, 1, 2, 2, 0, 0], [0, 0, 1, 1, 1, 2, 0, 0, -1], [0, 1, 1, 1, 1, 1, 0, -1, -1], [0, 1, 0, 0, 0, 1, -1, -1, -1], [0, 1, 0, 0, 0, -1, -1, -1, -1]])), 2)
+    simulate_agents(GameState(),6)
