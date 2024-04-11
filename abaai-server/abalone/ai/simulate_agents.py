1183bcf00836c6346f40039fe280e0b462cb587c
ankitahlwat <ankitahlwat54@gmail.com>
2024-04-11 02:43:29 -0700

    Training our boy

--- abaai-server/abalone/ai/simulate_agents.py
+++ abaai-server/abalone/ai/simulate_agents.py
@@ -1,7 +1,10 @@
 import time
+from random import choice
 
 from abalone.ai.cython.cython_clumping_agent import AlphaBetaPruningAgentAnkit
-from abalone.board import OptimizedBoard
+from abalone.ai.ankit_agent import AlphaBetaPruningAgentAnkit as AlphaBetaPruningAgentAnkit2
+from abalone.ai.state_space_optimized import StateSpaceGenerator
+from abalone.board import OptimizedBoard, BoardLayout
 from abalone.state import GameState, GameStateUpdate
 from abalone.ai.game_playing_agent_revamped_iterative_deepening import AlphaBetaPruningAgentIterative
 from abalone.ai.game_playing_agent_revamped_iterative_deeping_no_clumping import AlphaBetaPruningAgentIterativeNoClumping
@@ -9,10 +12,10 @@ def simulate_agents(game_state: GameState, max_moves: int):
     game_state = game_state
     # agent1 = AlphaBetaPruningAgentIterative(max_depth=4)
     agent1 = AlphaBetaPruningAgentAnkit(max_depth=4)
-    agent2 = AlphaBetaPruningAgentIterativeNoClumping(max_depth=4)
-
+    # agent2 = AlphaBetaPruningAgentIterativeNoClumping(max_depth=4)
+    agent2 = AlphaBetaPruningAgentAnkit2(max_depth=4)
     for i in range(max_moves):
-        if game_state.turn.value == 1:
+        if game_state.turn.value == 2:
             start = time.time()
             original_marbles = game_state.remaining_opponent_marbles
             original_opponent_marbles = game_state.remaining_player_marbles
@@ -25,11 +28,13 @@ def simulate_agents(game_state: GameState, max_moves: int):
             if game_state.remaining_opponent_marbles < original_opponent_marbles:
                 print(f'marbles knocked off')
         else:
+            start = time.time()
             original_marbles = game_state.remaining_opponent_marbles
             original_opponent_marbles = game_state.remaining_player_marbles
             move = agent2.iterative_deepening_search(game_state)
             game_state = GameStateUpdate(game_state, move).resulting_state
             print(f'white move {move}')
+            print(f'time taken: {time.time() - start}')
             if game_state.remaining_player_marbles < original_marbles:
                 print(f'marbles knocked off')
             if game_state.remaining_opponent_marbles < original_opponent_marbles:
@@ -38,9 +43,30 @@ def simulate_agents(game_state: GameState, max_moves: int):
     print(game_state.turn)
     print(game_state.remaining_player_marbles)
     print(game_state.remaining_opponent_marbles)
-    # agent1.write_t_table()
+    agent1.write_t_table()
     # agent2.write_t_table()
 
+def train_agent():
+    game_state = GameState(OptimizedBoard(BoardLayout.BELGIAN_DAISY.value))
+    agent1 = AlphaBetaPruningAgentAnkit(max_depth=4)
+    all_moves = StateSpaceGenerator.generate_all_possible_moves(game_state)
+    for i in range(len(sorted(all_moves))//10):
+        move = all_moves[i]
+        game_state = GameStateUpdate(game_state, move).resulting_state
+        for i in range(20):
+            best_move = agent1.iterative_deepening_search(game_state)
+            print(f'{game_state.turn.value} {best_move}')
+            game_state = GameStateUpdate(game_state, best_move).resulting_state
+        print(game_state.turn)
+        print(game_state.remaining_player_marbles)
+        print(game_state.remaining_opponent_marbles)
+        agent1.write_t_table()
+
+
+
 if __name__ == '__main__':
+    start = time.time()
     # simulate_agents(GameState(OptimizedBoard([[-1, -1, -1, -1, 0, 0, 0, 2, 2], [-1, -1, -1, 2, 0, 0, 0, 2, 2], [-1, -1, 0, 0, 2, 0, 2, 2, 0], [-1, 0, 0, 2, 1, 2, 2, 0, 0], [0, 0, 0, 1, 1, 2, 2, 0, 0], [0, 0, 1, 1, 1, 2, 0, 0, -1], [0, 1, 1, 1, 1, 1, 0, -1, -1], [0, 1, 0, 0, 0, 1, -1, -1, -1], [0, 1, 0, 0, 0, -1, -1, -1, -1]])), 2)
-    simulate_agents(GameState(),6)
+    # simulate_agents(GameState(OptimizedBoard(BoardLayout.BELGIAN_DAISY.value)),50)
+    # print(f'total time taken: {time.time() - start}')
+    train_agent()
