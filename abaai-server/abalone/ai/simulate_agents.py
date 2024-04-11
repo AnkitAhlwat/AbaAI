403b60c9188fdc8f4bab573a4da64c54169fb19d
ankitahlwat <ankitahlwat54@gmail.com>
2024-04-11 05:39:07 -0700

    Training our boy

--- abaai-server/abalone/ai/simulate_agents.py
+++ abaai-server/abalone/ai/simulate_agents.py
@@ -47,15 +47,15 @@ def simulate_agents(game_state: GameState, max_moves: int):
     # agent2.write_t_table()
 
 def train_agent():
-    game_state = GameState(OptimizedBoard(BoardLayout.BELGIAN_DAISY.value))
+    game_state = GameState(OptimizedBoard(BoardLayout.GERMAN_DAISY.value))
     agent1 = AlphaBetaPruningAgentAnkit(max_depth=4)
     all_moves = StateSpaceGenerator.generate_all_possible_moves(game_state)
     for i in range(len(sorted(all_moves))):
-        game_state = GameState(OptimizedBoard(BoardLayout.BELGIAN_DAISY.value))
+        game_state = GameState(OptimizedBoard(BoardLayout.GERMAN_DAISY.value))
         agent1 = AlphaBetaPruningAgentAnkit(max_depth=4)
         move = all_moves[i]
         game_state = GameStateUpdate(game_state, move).resulting_state
-        for i in range(5):
+        for i in range(6):
             best_move = agent1.iterative_deepening_search(game_state)
             print(f'{game_state.turn.value} {best_move}')
             game_state = GameStateUpdate(game_state, best_move).resulting_state
