48c0dd2dfe6b6c18e879729d8686d8cc89feb8ee
ankitahlwat <ankitahlwat54@gmail.com>
2026-04-14 15:26:54 -0700

    Remove unused files

--- abaai-client/src/constants/boardLayouts.js
+++
@@ -1,8 +0,0 @@
-// Enum for possible board layouts
-const BoardLayouts = {
-    DEFAULT: "Default",
-    BELGIAN_DAISY: "Belgian Daisy",
-    GERMAN_DAISY: "German Daisy",
-}
-
-export { BoardLayouts };
--- abaai-client/src/constants/spaceStates.js
+++
@@ -1,9 +0,0 @@
-// Enum for possible tiles of game board representation
-const SpaceStates = {
-    EMPTY: 0,
-    BLACK: 1,
-    WHITE: 2,
-    NONE: -1,
-}
-
-export default SpaceStates;
